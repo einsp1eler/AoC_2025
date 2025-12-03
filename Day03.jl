8e74884d7554e9e3942092d85989012200fea48f
einspieler <alexis66@gmx.at>
2025-12-03 15:18:20 +0100

    Day 3

---
+++ Day03.jl
@@ -0,0 +1,17 @@
+function solve(banks, x)
+    ret = 0
+    for bank in banks
+        nums = [maximum(bank[1:end-x])]
+        for i in x-1:-1:0
+            bank = bank[findfirst(==(nums[x-i]), bank)+1:end]
+            push!(nums, maximum(bank[1:end-i]))
+        end
+        ret += sum(parse(Int, join(nums)))
+    end
+    return ret
+end
+
+banks = readlines("Input/Day03.txt")
+
+println(solve(banks, 1))
+println(solve(banks, 11))
